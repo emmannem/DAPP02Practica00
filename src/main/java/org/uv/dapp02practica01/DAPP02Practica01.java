b243c747c2e8df74ed0b97b916a753d3d0af78f6
Josemmanuel <emmanuelriv15@gmail.com>
2024-03-03 19:35:47 -0600

    add base code

---
+++ src/main/java/org/uv/dapp02practica01/DAPP02Practica01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.uv.dapp02practica01;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DAPP02Practica01 {
+
+    public static void main(String[] args) {
+        ConexionDB con = ConexionDB.getInstance();
+    }
+}
